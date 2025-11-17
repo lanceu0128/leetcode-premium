6dfdc1a60f04670b8f1cf36618b6230eb4eb3a6e
Lance <81938513+lanceu0128@users.noreply.github.com>
2025-11-16 23:43:06 -0500

    make 253 better lol

--- 253.py
+++ 253.py
@@ -4,11 +4,10 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         heap = []
         intervals.sort(key=lambda x: x[0])
-        heapq.heappush(heap, intervals[0][1])
-        output = 1
-        curr = 1
+        output = 0
+        curr = 0
 
-        for interval in intervals[1:]:
+        for interval in intervals:
             # free up all available rooms
             if len(heap) and heap[0] <= interval[0]:
                 heapq.heappop(heap)
